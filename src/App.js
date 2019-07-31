a6cebf77890d13d8f5ec86168c33b0de8a9f43d6
Harvey Gomez <hmgomez90@gmail.com>
2019-07-31 22:40:55 +0100

    Setup testing with Jest and add the ESLint config for it.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React from "react";
 import { render } from "react-dom";
 
 const App = () => {
-  return (
-    <div>
-      <h1>App rendered correctly.</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>App rendered correctly.</h1>
+        </div>
+    );
 };
 
 render(<App />, document.getElementById("root"));
-
---
+++ src/App.test.js
@@ -0,0 +1,5 @@
+describe('Placeholder test', () => {
+    it('Should render correctly', () => {
+        expect(1 + 1).toBe(2);
+    })
+});
